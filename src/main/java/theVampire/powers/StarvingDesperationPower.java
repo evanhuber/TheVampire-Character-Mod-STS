f40ba6959de8817d320931be1f5d1a3992f7789a
Evan <77908032+evanhuber@users.noreply.github.com>
2021-02-09 15:31:24 -0600

    Update StarvingDesperationPower.java

--- src/main/java/theVampire/powers/StarvingDesperationPower.java
+++ src/main/java/theVampire/powers/StarvingDesperationPower.java
@@ -21,7 +21,6 @@ import theVampire.util.TextureLoader;
 
 import static theVampire.DefaultMod.makePowerPath;
 
-//Gain 1 dex for the turn for each card played.
 
 public class StarvingDesperationPower extends AbstractPower implements CloneablePowerInterface {
     public AbstractCreature source;
@@ -32,8 +31,6 @@ public class StarvingDesperationPower extends AbstractPower implements Cloneable
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
     private int growingDamage = 5;
 
-    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
-    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
     private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
     private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
 
@@ -49,7 +46,6 @@ public class StarvingDesperationPower extends AbstractPower implements Cloneable
         type = PowerType.BUFF;
         isTurnBased = false;
 
-        // We load those txtures here.
         this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
         this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
 
@@ -96,7 +92,6 @@ public class StarvingDesperationPower extends AbstractPower implements Cloneable
 
     }
 
-    // Update the description when you apply this power. (i.e. add or remove an "s" in keyword(s))
     @Override
     public void updateDescription() {
         description = DESCRIPTIONS[0] + growingDamage + DESCRIPTIONS[1];
