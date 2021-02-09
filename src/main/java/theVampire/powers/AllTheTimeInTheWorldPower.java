21ef319ed7b96b7a1e8332fabefce94e89161ddc
Evan <77908032+evanhuber@users.noreply.github.com>
2021-02-09 15:23:12 -0600

    Update AllTheTimeInTheWorldPower.java

--- src/main/java/theVampire/powers/AllTheTimeInTheWorldPower.java
+++ src/main/java/theVampire/powers/AllTheTimeInTheWorldPower.java
@@ -19,7 +19,7 @@ import theVampire.util.TextureLoader;
 
 import static theVampire.DefaultMod.makePowerPath;
 
-//Gain 1 dex for the turn for each card played.
+
 
 public class AllTheTimeInTheWorldPower extends AbstractPower implements CloneablePowerInterface {
     public AbstractCreature source;
@@ -30,8 +30,7 @@ public class AllTheTimeInTheWorldPower extends AbstractPower implements Cloneabl
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
-    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
-    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+
     private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
     private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
 
@@ -46,7 +45,7 @@ public class AllTheTimeInTheWorldPower extends AbstractPower implements Cloneabl
         type = PowerType.BUFF;
         isTurnBased = false;
 
-        // We load those txtures here.
+        
         this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
         this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
 
